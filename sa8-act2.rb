89573c3ec7e1e268c1ee14e1308cd82144d59b73
Adrien Jones <tjones69@memphis.edu>
2024-02-19 14:04:36 -0600

    Added Appliance class and subclasses with inheritance

--- sa8-act2.rb
+++ sa8-act2.rb
@@ -52,6 +52,27 @@ class User
 end
 
 user1 = User.new("TJMarie")
-user1.username = ""
-user1.username = "Tatiana"
-puts user1.inspect
+
+puts "Problem 4"
+
+class Appliance
+  def show_info
+    puts "This is a household appliance"
+  end
+end
+
+class Refrigerator < Appliance
+  def fridge
+    puts "This is a refrigerator"
+  end
+end
+
+class Microwave < Appliance
+  def microwave
+    puts "This is a microwave"
+  end
+end
+
+fridge1 = Refrigerator.new
+fridge1.fridge
+fridge1.show_info
